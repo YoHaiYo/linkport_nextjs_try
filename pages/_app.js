be1d149e41710bf23ad87b3b6a16178b771a139a
YoHaiYo <12si47bun@naver.com>
2024-06-06 19:21:18 +0900

    폰트어썸 _app.js 없이 배포되는지 확인용

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,8 @@
-import { config } from "@fortawesome/fontawesome-svg-core";
-import "@fortawesome/fontawesome-svg-core/styles.css";
-config.autoAddCss = false;
+/// 이거 주석해도 폰트어썸 아이콘 제대로 배포되는지 확인해보기
+// import { config } from "@fortawesome/fontawesome-svg-core";
+// import "@fortawesome/fontawesome-svg-core/styles.css";
+// config.autoAddCss = false;
 
-export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />;
-}
+// export default function App({ Component, pageProps }) {
+//   return <Component {...pageProps} />;
+// }
